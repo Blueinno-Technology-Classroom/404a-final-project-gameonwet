552d5df0a68d4b59f07fb6eca78fe27d2f20f258
hadpfapdf <calebcheung1223@gmail.com>
2024-06-21 10:45:58 +0800

    Co-authored-by: Ben-Blueinno <Ben-Blueinno@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ HEIGHT = 768
 
 player = Actor("player")
 player.pos = (WIDTH / 2, HEIGHT / 2)
+player.pre_angles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 leaves = []
 leaf_CD = 0
@@ -31,7 +32,8 @@ def update():
     if leaf_CD >= 240:
         for i in range(leaf_num):
             leaf = Actor("leaf")
-            leaf.scale = random.randint(5, 9) / 10
+            leaf.final_size = random.randint(5, 9) / 10
+            leaf.scale = leaf.final_size / 2
             leaf.x = random.randint(0, WIDTH)
             leaf.y = random.randint(0, HEIGHT)
             leaf.pe = 0
@@ -46,13 +48,15 @@ def update():
         if player.distance_to(leaf) <= blow_range:
             leaf.point_towards(player)
             leaf.pe = blow_power
+        if leaf.scale < leaf.final_size:
+            leaf.scale += 0.05
             
 
-
+                                                                                                                                
 
         if leaf.pe > 0:
             leaf.move_forward(-leaf.pe)
-            leaf.pe -= 1
+            leaf.pe -= 0.5
         
 
 
@@ -77,7 +81,10 @@ def update():
 
 def on_mouse_move(pos):
     player.pos = pos
-    player.angle = player.angle_to(prev_mouse_pos) - 180
+    player.pre_angles.append(player.angle_to(prev_mouse_pos) - 180)
+    player.pre_angles = player.pre_angles[1:]
+    player.angle = sum(player.pre_angles)/len(player.pre_angles)
+    # player.angle = int((player.angle_to(prev_mouse_pos) - 180)/45)*45
     prev_mouse_pos[0] = pos[0]
     prev_mouse_pos[1] = pos[1]
 
